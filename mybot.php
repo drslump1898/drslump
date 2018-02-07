3867646b4cfcb217ea06366565a520a88600df34
drslump1898 <36221901+drslump1898@users.noreply.github.com>
2018-02-07 14:39:59 +0700

    Update mybot.php

--- mybot.php
+++ mybot.php
@@ -35,6 +35,13 @@ if (!is_null($events['events'])) {
 					'text' => 'คือเครื่องคำนวณ'
 				];
 			}
+			else if($text == 'ประยุทธ์'){
+				// Build message to reply back
+				$messages = [
+					'type' => 'text',
+					'text' => 'คนหัวควย'
+				];
+			}
 		
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
